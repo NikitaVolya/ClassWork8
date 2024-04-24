284458797f845ad0dcf3e86409c0b94f78d613ec
NikitaVolia <volanskijnikita2@gmail.com>
2024-04-24 19:21:10 +0200

    add const to print method

--- Pair.h
+++ Pair.h
@@ -10,6 +10,6 @@ private:
 	SecondType second;
 
 public:
-	void print(char end = '\n') { std::cout << first << " " << second << end; };
+	void print(char end = '\n') const { std::cout << first << " " << second << end; };
 	Pair(FirstType pFirst, SecondType pSecond) : first(pFirst), second(pSecond) {};
 };
